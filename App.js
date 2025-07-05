1542c1ab2fc6595e76e0e8f5c399e368c40d318a
MounaBM <mounabm011@gmail.com>
2025-07-05 16:17:32 +0530

    Episode-01

---
+++ App.js
@@ -0,0 +1,15 @@
+ 
+ const parent = React.createElement("div" , {id:"parent"},
+    [React.createElement("div" , {id:"child1"},
+        [React.createElement("h1",{id:"heading"},"Hi World From React , Am h1 tag"),
+        React.createElement("h2",{id:"heading"},"Hi World From React , Am h2 tag")]
+    ),
+    React.createElement("div" , {id:"child2"},
+        [React.createElement("h1",{id:"heading"},"Hi World From React , Am h1 tag"),
+        React.createElement("h2",{id:"heading"},"Hi World From React , Am h2 tag")]
+    )]
+ );
+
+ const root = ReactDOM.createRoot(document.getElementById("root"));
+
+  root.render(parent)
