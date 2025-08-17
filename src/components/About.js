8cc3a11d97e1c255c4f8cd52d52d9c7b424d7dd0
MounaBM <mounabm011@gmail.com>
2025-08-17 21:06:35 +0530

    Episode-08

--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,27 @@
-const About = ()=>{
-    return (
+import { Component } from "react";
+import User from "./User";
+import UserClass from "./UserClass";
+import { Component } from "react";
+
+class About extends Component{
+    constructor(probs){
+        super(probs)
+        console.log("Parent constructor")
+    }
+
+    componentDidMount(){
+        console.log("Parent componentDidMount")
+    }
+
+    render(){
+        console.log("Parent render")
+        return (
         <div>
             <h1>About Us</h1>
+            <UserClass name={"First"} location={"Mysore cl"} contact={"@mounabm011 cl"}/>
         </div>
     )
+    }
 }
 
 export default About;
---
+++ src/components/UserClass.js
@@ -0,0 +1,47 @@
+import React from "react";
+
+class UserClass extends React.Component{
+    constructor(props){
+        super(props)
+        this.state = {
+            userInfo : {
+                name:'Dummy',
+                location:'Dummy'
+            }
+        }
+        // console.log(this.props.name +"Child constructor")
+    }
+
+    async componentDidMount(){
+        const data = await fetch("https://api.github.com/users/akshaymarch7");
+
+        const json = await data.json();
+
+        this.setState ({
+            userInfo : json
+        })
+    }
+
+    componentDidUpdate(){
+        console.log("component did update")
+    }
+
+    componentWillUnmount(){
+        console.log("component will unmount")
+    }
+
+    render(){
+        console.log(this.state.userInfo)
+    const {name , location , avatar_url} = this.state.userInfo
+    // console.log(this.props.name  + "Child render")
+    return (
+        <div className="user-card">
+            <img src={avatar_url}/>
+            <h2>Name: {name}</h2>
+            <h3>Location: {location}</h3>
+        </div>
+    )
+    }
+}
+
+export default UserClass;
