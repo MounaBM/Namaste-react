d604c6125be5575a5ed716a65064bc6127954de5
MounaBM <mounabm011@gmail.com>
2025-08-19 21:12:50 +0530

    Episode-10

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,18 +9,18 @@ const Header = () =>{
    const onlineStatus = useOnlineStatus();
 
    return (
-      <div className='header'>
+      <div className='flex justify-between bg-pink-200 shadow-md border border-s-violet-500'>
          <div className='logo-container'>
-         <img className="logo" src={LOGO_URL} />
+         <img className="w-28 shadow-md" src={LOGO_URL} />
          </div>
-         <div className='navItems'>
-            <ul>
-               <li>Online Status : {onlineStatus ? "💕" : "🤦‍♀️"} </li>
-               <li><Link to="/">Home</Link></li>
-               <li><Link to="/about">About Us</Link></li>
-               <li><Link to="/contact">Contact Us</Link></li>
-               <li><Link to="/grocery">Grocery</Link></li>
-               <li>Cart</li>
+         <div className='flex justify-center justify-items-center mr-8'>
+            <ul className='flex'>
+               <li className='px-3 flex items-center'>Online Status : {onlineStatus ? "💕" : "🤦‍♀️"} </li>
+               <li className='px-3 flex items-center'><Link to="/">Home</Link></li>
+               <li className='px-3 flex items-center'><Link to="/about">About Us</Link></li>
+               <li className='px-3 flex items-center'><Link to="/contact">Contact Us</Link></li>
+               <li className='px-3 flex items-center'><Link to="/grocery">Grocery</Link></li>
+               <li className='px-3 flex items-center'>Cart</li>
                <button className='loginBtn' onClick={()=>{
                     btnName === "Login" ? setBtnName('Logout') : setBtnName('Login');
                }}>{btnName}</button>
--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -4,10 +4,10 @@ const RestroCards = (probs) =>{
    const {resData} = probs;
    const {cloudinaryImageId , name ,cuisines ,avgRating , costForTwo , sla} = resData?.info
    return (
-      <div className='res-card' style={{backgroundColor : "#f0f0f0"}}>
-         <img className='res-logo' src={CDN_URL +
+      <div className='w-52 h-auto break-words p-2 m-2 bg-gray-200 rounded-lg hover:bg-slate-300'>
+         <img className='w-46 flex rounded-lg' src={CDN_URL +
           cloudinaryImageId}/>
-         <h3>{name}</h3>
+         <h3 className='font-bold'>{name}</h3>
          <h4>{cuisines.join(', ')}</h4>
          <h4>{avgRating} starts</h4>
          <h4>{costForTwo} </h4>
